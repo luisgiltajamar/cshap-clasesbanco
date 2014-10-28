96c18198590b1bb4a5b67324ac748091f6a531f3
luisgiltajamar <luis.gil@tajamar365.com>
2014-10-28 12:33:25 +0100

    Con el main

--- Banco/Program.cs
+++ Banco/Program.cs
@@ -10,6 +10,20 @@ namespace Banco
     {
         static void Main(string[] args)
         {
+            var cc = new CuentaCorriente("Luis", 1000);
+            var cc2 = new CuentaCorriente("Pepe", 3000, true, 1500);
+            var ca = new CuentaAhorro("Maria", 5000, true, 0.02);
+
+            Console.WriteLine(cc.Sacar(2000));
+            Console.WriteLine(cc2.Sacar(4000));
+            Console.WriteLine(ca.Sacar(2000));
+
+
+            cc.Detalles();
+            cc2.Detalles();
+            ca.Detalles();
+
+            Console.ReadLine();
         }
     }
 }
